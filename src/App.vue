2981f4e874ca25dcfc42928a58efc32cbd8df185
unknown <omarkhaled12oki@gmail.com>
2024-07-24 19:42:51 +0300

    Upload the first version of the task

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,128 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import carouselStore from './stores/cards'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+// import swiper
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination, Autoplay, Navigation } from 'swiper/modules'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/swiper-bundle.css'
+
+let cardsStore = carouselStore()
+
+let customerName = ref('')
+let customerJobTitle = ref('')
+let customerReivew = ref('')
+let modules = [Pagination, Navigation, Autoplay]
+
+const handleSubmit = () => {
+  let newReview
+  if (customerReivew.value !== '' && customerName.value !== '')
+    newReview = {
+      userName: customerName.value,
+      jobTitle: customerJobTitle.value,
+      review: customerReivew.value,
+      numOfStars: 5
+    }
+  cardsStore.addReview(newReview)
+
+  customerName.value = ''
+  customerJobTitle.value = ''
+  customerReivew.value = ''
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Krokology Academy</h1>
   </header>
-
   <main>
-    <TheWelcome />
+    <div class="container">
+      <h2>What Our Customer Said</h2>
+      <div class="swiper-container">
+        <div class="carousel">
+          <swiper
+            ref="swipper"
+            :slides-per-view="3"
+            :space-between="10"
+            :pagination="{ clickable: true }"
+            :navigation="true"
+            :loop="true"
+            :autoplay="{ delay: 2000, disableOnInteraction: false }"
+            :modules="modules"
+          >
+            <swiper-slide
+              class="card swiper-slide"
+              v-for="card in cardsStore.getCards"
+              :key="card.id"
+            >
+              <div class="content">
+                <div class="icons">
+                  <FontAwesomeIcon
+                    :icon="faStar"
+                    class="star"
+                    v-for="star in card.numOfStars"
+                    :key="star"
+                  />
+                </div>
+                <div class="text">
+                  <p>{{ card.review }}</p>
+                </div>
+              </div>
+              <div class="user-info">
+                <div class="img">
+                  <img
+                    v-if="card.img"
+                    :src="`src/assets/${card.img}`"
+                    :alt="`${card.userName} image`"
+                    class="user-img"
+                  />
+                  <img src="@/assets/user.png" alt="user image" v-else />
+                </div>
+                <div class="info">
+                  <h3>{{ card.userName }}</h3>
+                  <p>{{ card.jobTitle }}</p>
+                </div>
+              </div>
+            </swiper-slide>
+            <template v-slot:pagination>
+              <div class="swiper-pagination"></div>
+            </template>
+            <template v-slot:button-prev>
+              <div class="swiper-button-prev"></div>
+            </template>
+            <template v-slot:button-next>
+              <div class="swiper-button-next"></div>
+            </template>
+          </swiper>
+        </div>
+      </div>
+      <div class="new-review">
+        <h2>Add / Edit Review</h2>
+        <form @submit.prevent="handleSubmit">
+          <input type="text" placeholder="Name" v-model="customerName" />
+          <input type="text" placeholder="Job Title" v-model="customerJobTitle" />
+          <textarea placeholder="Review" v-model="customerReivew"></textarea>
+          <button type="submit" class="submit">Save</button>
+        </form>
+      </div>
+    </div>
   </main>
 </template>
 
-<style scoped>
+<style lang="scss">
 header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  text-align: center;
+  margin: 20px auto;
+  h1 {
+    font-size: 22px;
+    font-weight: 700;
+    font-family: 'Poppins';
+    line-height: 29.92px;
+    color: #1a1a1a;
   }
 }
 </style>
---
+++ src/stores/cards.js
@@ -0,0 +1,65 @@
+import { defineStore } from 'pinia'
+
+const carouselStore = defineStore('useTaskStore', {
+  state: () => ({
+    cards: [
+      {
+        id: 1,
+        userName: 'Anthony',
+        jobTitle: 'ABC Company',
+        review:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porttitor vestibulum odio, eget convallis enim euismod ut. Integer sollicitudin at nibh maximus molestie. Donec vitae diam et sem posuere lobortis nec nec lacus. Proin id enim nec erat volutpat mattis vel ut felis.',
+        img: 'customer-1.png',
+        numOfStars: 3
+      },
+      {
+        id: 2,
+        userName: 'Anthony',
+        jobTitle: 'ABC Company',
+        review:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porttitor vestibulum odio, eget convallis enim euismod ut. Integer sollicitudin at nibh maximus molestie. Donec vitae diam et sem posuere lobortis nec nec lacus. Proin id enim nec erat volutpat mattis vel ut felis.',
+        img: 'customer-2.png',
+        numOfStars: 5
+      },
+      {
+        id: 3,
+        userName: 'Anthony',
+        jobTitle: 'ABC Company',
+        review:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porttitor vestibulum odio, eget convallis enim euismod ut. Integer sollicitudin at nibh maximus molestie. Donec vitae diam et sem posuere lobortis nec nec lacus. Proin id enim nec erat volutpat mattis vel ut felis.',
+        img: 'customer-3.png',
+        numOfStars: 4
+      },
+      {
+        id: 4,
+        userName: 'Anthony',
+        jobTitle: 'ABC Company',
+        review:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porttitor vestibulum odio, eget convallis enim euismod ut. Integer sollicitudin at nibh maximus molestie. Donec vitae diam et sem posuere lobortis nec nec lacus. Proin id enim nec erat volutpat mattis vel ut felis.',
+        img: 'customer-2.png',
+        numOfStars: 5
+      },
+      {
+        id: 5,
+        userName: 'Anthony',
+        jobTitle: 'ABC Company',
+        review:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porttitor vestibulum odio, eget convallis enim euismod ut. Integer sollicitudin at nibh maximus molestie. Donec vitae diam et sem posuere lobortis nec nec lacus. Proin id enim nec erat volutpat mattis vel ut felis.',
+        img: 'customer-3.png',
+        numOfStars: 4
+      }
+    ]
+  }),
+  getters: {
+    getCards: (state) => {
+      return state.cards
+    }
+  },
+  actions: {
+    addReview(review) {
+      this.cards.push(review)
+    }
+  }
+})
+
+export default carouselStore
